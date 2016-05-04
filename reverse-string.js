75b004c45f53fcbae73d2ecb8e739447908f18fb
Sheng <sheng.di@citrix.com>
2016-05-04 12:04:28 -0700

    Reverse String

--- reverse-string.js
+++ reverse-string.js
@@ -2,23 +2,10 @@
  * @param {string} s
  * @return {string}
  */
-function swap(start, end, s) {
-    var temp;
-    var res = s.split("");
-    temp = res[end];
-    res[end] = res[start];
-    res[start] = temp;
-    return res.join('');
-}
-
 var reverseString = function(s) {
-    var len = s.length;
-    if(len < 2) {
+    if(s.length < 2) {
         return s;
     } else {
-        for(var i=0;i<len/2;i++) {
-            s = swap(i, len-i-1,s);
-        }
-        return s;
+        return s.split('').reverse().join('');
     }
 };

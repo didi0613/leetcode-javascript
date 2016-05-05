d1d23ad9324c74cb584dcc7f6e61c46cee611a30
gina0613 <sdi@usc.edu>
2016-05-05 13:13:31 -0700

    Add digits

--- add-digits.js
+++ add-digits.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var addDigits = function(num) {
+    if(num < 10) {
+        return num;
+    }
 
+    return addDigits(num.toString().split('').reduce((a, b) => Number(a) + Number(b), 0));
 };
+
+/*
+ * Summarize
+ * - Pay attention to the string and number transition.
+ *
+ * - reduce (callback, initial_value)
+ *   The reduce() method applies a function against an accumulator
+ *   and each value of the array (from left-to-right) to reduce it to a single value.
+ */

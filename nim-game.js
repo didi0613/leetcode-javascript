bde8cdfe6f9e80928a7a09b97688b2a5a692e9c9
gina0613 <sdi@usc.edu>
2016-05-05 12:39:43 -0700

    Nim Game

--- nim-game.js
+++ nim-game.js
@@ -7,5 +7,11 @@ var canWinNim = function (n) {
 };
 
 /**
- * Summarize
+ * Analyse
+ * n = [1,3], first always win
+ * n = 4, second always win, cause whatever first picks, always transform to n = [1,3] case.
+ * n = [5,7], first always win, cause first can force the second always met n = 4 case.
+ * n = 8, second always win, cause whatever first picks, always transform to n = [5,7] case.
+ * ...
+ * As far as n is not multiple of 4, first wins.
  */

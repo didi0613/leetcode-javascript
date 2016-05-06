2da25f7e36b0f77e0c6797c5c9456f14ab4a6829
gina0613 <sdi@usc.edu>
2016-05-06 16:01:37 -0700

    Invert Binary Tree

---
+++ invert-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root) {
+        var temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    } else {
+        return root;
+    }
+};

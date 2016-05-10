540642df1f48a1e974ea903683b141a2a147253a
gina0613 <sdi@usc.edu>
2016-05-09 18:02:52 -0700

    Move Zeroes

--- move-zeroes.js
+++ move-zeroes.js
@@ -3,5 +3,31 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    var len = nums.length;
+    var runner = 0, newIndex = 0;
 
+    while(runner < len) {
+        while(nums[runner] === 0) {
+            runner++;
+        }
+
+        if(runner < len) {
+            nums[newIndex] = nums[runner];
+            runner++;
+            newIndex++;
+        }
+    }
+
+    for(var i = newIndex; i < len; i++) {
+        nums[i] = 0;
+    }
 };
+
+/*
+ * Analyze:
+ * Two pointers classic problem.
+ * Runner pointer find all the non-zeroes numbers,
+ * Replace the new index position.
+ *
+ * Be careful with the index scope along with the pointer moving.
+ */

571cd7b0faa4c6e3b708415cbb8795af353ff5ac
gina0613 <sdi@usc.edu>
2016-05-09 18:10:40 -0700

    Delete node in a linked list

--- delete-node-in-a-linked-list.js
+++ delete-node-in-a-linked-list.js
@@ -10,5 +10,15 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-
+    if(node.next !== null) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    } else {
+        node = null;
+    }
 };
+
+/*
+ * Summarize:
+ * Distinguish LinkedList tail node and regular node
+ */

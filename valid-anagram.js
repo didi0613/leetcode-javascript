206195737b1215128f060de55849a9e007a0609d
gina0613 <sdi@usc.edu>
2016-05-09 18:22:54 -0700

    Valid anagram

---
+++ valid-anagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return s.split("").sort().join("") == t.split("").sort().join("");
+};
+
+/*
+* Summarize:
+* sort only works with array
+*/

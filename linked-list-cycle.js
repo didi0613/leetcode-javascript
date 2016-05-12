9f52406225f0ff2d89f21596134130a9df8ed531
gina0613 <sdi@usc.edu>
2016-05-12 16:21:35 -0700

    Linked List Cycle

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -11,5 +11,25 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    if(head === null || head.next === null) {
+        return false;
+    }
 
+    var slow = head;
+    var fast = head.next.next;
+
+    while(slow && fast) {
+        if(slow === fast) {
+            return true;
+        } else {
+            if(slow.next && fast.next && fast.next.next) {
+                slow = slow.next;
+                fast = fast.next.next;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return false;
 };

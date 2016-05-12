b143330bee8af9b96a2747393a142e92b4e57e05
gina0613 <sdi@usc.edu>
2016-05-12 12:38:35 -0700

    Number of 1 bits

---
+++ number-of-1-bits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    var ret = 0;
+
+    var arr = n.toString(2).split('').sort();
+    for(var i = arr.length-1; i>=0;i--){
+        if(arr[i] === '1') {
+            ret++;
+        } else {
+            break;
+        }
+    }
+
+    return ret;
+};
+
+/*
+* Transform decimal to n-ary bigit number.toString(n);
+*/

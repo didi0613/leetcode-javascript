62c08e68bbcd9eef5c859d16160e052bc8c751d4
gina0613 <sdi@usc.edu>
2016-05-12 13:48:13 -0700

    Climbing stairs

---
+++ climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    var ret = [];
+    ret[0] = 0;
+    ret[1] = 1;
+    ret[2] = 2;
+
+    for(var i=3;i<=n;i++) {
+        ret[i] = ret[i-1] + ret[i-2];
+    }
+
+    return ret[n];
+};

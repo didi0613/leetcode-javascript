5823083c86bb3c2cf5a4397069ff3a99e7ae8350
gina0613 <sdi@usc.edu>
2016-05-12 17:22:48 -0700

    Ugly Number

---
+++ ugly-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isUgly = function(num) {
+    while(num > 1) {
+        if(num%2 === 0) {
+            num = num/2;
+        } else if(num%5 === 0) {
+            num = num/5;
+        } else if(num%3 === 0) {
+            num = num/3;
+        } else {
+            return false;
+        }
+    }
+
+    if(num === 1) {
+        return true;
+    } else {
+        return false;
+    }
+};

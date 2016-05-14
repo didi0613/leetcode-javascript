61964669bbef095f69f832f8fb8d46435c5af0d6
gina0613 <sdi@usc.edu>
2016-05-13 23:55:23 -0700

    Best Time to buy and sell stocks

---
+++ best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var len = prices.length;
+    var ret = 0;
+
+    for(var i=0;i<len;i++) {
+        var sellMax = 0;
+        for(var j = i+1;j<len;j++) {
+            sellMax = Math.max(sellMax, prices[j]);
+        }
+        ret = Math.max(ret, sellMax-prices[i]);
+    }
+
+    return ret;
+};

c17baddb01924801f4daae9cd3b48ebf4fb3beac
gina0613 <sdi@usc.edu>
2016-05-14 11:35:09 -0700

    Swap nodes in pairs

---
+++ merge-two-sorted-lists.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    var start1 = l1, start2 = l2;
+    var ret = [];
+
+    while(start1 && start2) {
+        if(start1.val < start2.val) {
+            ret.push(start1.val);
+            start1 = start1.next;
+        } else {
+            ret.push(start2.val);
+            start2 = start2.next;
+        }
+    }
+
+    while(start1) {
+            ret.push(start1.val);
+            start1 = start1.next;
+        }
+
+    while(start2) {
+            ret.push(start2.val);
+            start2 = start2.next;
+        }
+
+    return ret;
+};

c3a0c9aa0fcbdbfa723f44d86d82f91be4ea00ba
gina0613 <sdi@usc.edu>
2016-05-14 11:58:31 -0700

    Swap nodes in pairs

---
+++ swap-nodes-in-pairs.js
@@ -0,0 +1,8 @@
+
+/* Summarize
+* Dummy Node 可以认为是一个假的头结点。
+* 我们可以构造一个Dummy Node ，使他的next指针指向Head node。
+* 使用Dummy Node目的是，在单向链表中，保正head不会再删除操作中丢失。
+* 此外，比较特殊的方法是用来进行删除head。
+* Dummy Node可以处理对于head Node变化的情况。
+*/

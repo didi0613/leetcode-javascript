7e34c04acd9f97345d1bc44b849a3cd08ebb19b9
gina0613 <sdi@usc.edu>
2016-05-16 12:23:34 -0700

    House Robber

---
+++ house-robber.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    var ret = 0;
+    var len = nums.length;
+    if(len === 0) {
+        return ret;
+    } else if(len === 1) {
+        return nums[0];
+    }
+
+    var f = [];
+    f[0] = nums[0];
+    f[1] = Math.max(nums[0], nums[1]);
+    ret = Math.max(f[0], f[1]);
+
+    for(var i = 2; i < len; i++) {
+        f[i] = Math.max(ret, f[i-2]+nums[i]);
+        ret = Math.max(ret, f[i]);
+    }
+
+    return f[len-1];
+};

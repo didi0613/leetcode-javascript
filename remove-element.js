c5b07362028f0e9c8e17a62312c9b70afa73e5c5
gina0613 <sdi@usc.edu>
2016-05-16 12:59:59 -0700

    Remove element

---
+++ remove-element.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var removeNum = 0;
+    var len = nums.length;
+
+    if(len === 0) {
+        return 0;
+    }
+
+    for(var i=0;i<len;i++) {
+        if(nums[i] === val) {
+            removeNum++;
+            var mark = i;
+            while(mark < len) {
+                nums[mark] = nums[mark+1];
+                mark++;
+            }
+            i--;
+        }
+    }
+
+    var removeNumDup = removeNum;
+    for(var j = len-1; removeNumDup>0 && j>=0;j--) {
+        nums[j] = null;
+        removeNumDup--;
+    }
+
+    return len-removeNum;
+
+};

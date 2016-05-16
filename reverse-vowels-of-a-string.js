bb064e58387a0d72ad4b4af2d8c238a5e2e2497d
gina0613 <sdi@usc.edu>
2016-05-16 12:07:05 -0700

    Reverse vowels of a string

--- reverse-vowels-of-a-string.js
+++ reverse-vowels-of-a-string.js
@@ -3,5 +3,28 @@
  * @return {string}
  */
 var reverseVowels = function(s) {
+    var len = s.length;
+    var start = 0, end = len-1;
+    var vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    var sarray = s.split('');
 
+    while(start < end) {
+        while(start < len && vowels.indexOf(sarray[start]) === -1) {
+            start++;
+        }
+
+        while(end >=0 && vowels.indexOf(sarray[end]) === -1) {
+            end--;
+        }
+
+        if(start < end) {
+            var temp = sarray[end];
+            sarray[end] = sarray[start];
+            sarray[start] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    return sarray.join('');
 };

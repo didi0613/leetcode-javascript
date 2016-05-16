6312e9764b492840611b92805688278209dd07ce
gina0613 <sdi@usc.edu>
2016-05-15 22:55:13 -0700

    Swap nodes in pairs

--- swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs.js
@@ -1,3 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function swapnode(h,r1,r2){
+    var next = r2.next;
+    h.next = r2;
+    r2.next = r1;
+    r1.next = next;
+
+    return r1;
+}
+
+var swapPairs = function(head) {
+    var newhead = new ListNode(-1);
+    newhead.next = head;
+    start = newhead;
+
+    while(start.next && start.next.next) {
+        start = swapnode(start,start.next,start.next.next);
+    }
+
+
+    return newhead.next;
+};
 
 /* Summarize
 * Dummy Node 可以认为是一个假的头结点。

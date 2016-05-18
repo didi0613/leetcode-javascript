fd8a3224d4ce78c106da931de980918f39985150
gina0613 <sdi@usc.edu>
2016-05-18 13:44:05 -0700

    Symmetric Tree

---
+++ symmetric-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+ function Symmetric(left,right) {
+     if(left === null && right === null) {
+         return true;
+     } else if(left !== null && right !== null) {
+         if(left.val === right.val) {
+             return Symmetric(left.left, right.right) && Symmetric(left.right, right.left);
+        } else {
+             return false;
+        }
+     } else {
+         return false;
+     }
+ }
+
+var isSymmetric = function(root) {
+    if(root === null) {
+        return true;
+    }
+
+    return Symmetric(root.left, root.right);
+};

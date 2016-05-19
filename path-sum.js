6cedb9a3d82fa41e04ccda46f349c43e17ad5755
gina0613 <sdi@usc.edu>
2016-05-18 18:48:32 -0700

    Path Sum

---
+++ path-sum.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if(root === null) {
+        return false;
+    }
+
+    if(root.val === sum && !root.left && !root.right) {
+        return true;
+    }
+
+    return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
+};

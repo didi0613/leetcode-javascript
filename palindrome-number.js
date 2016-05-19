d1ef3de001226e8db76902936e172c4314ee3ec8
gina0613 <sdi@usc.edu>
2016-05-18 18:35:52 -0700

    Palindrome Number

---
+++ palindrome-number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x<0) {
+        return false;
+    } else if (x>=0 && x<10) {
+        return true;
+    }
+
+    var x_str = x.toString();
+    var len = x_str.length;
+    var start = 0, end = len-1;
+
+    while(start <= end) {
+        if(x_str.charAt(start) === x_str.charAt(end)) {
+            start++;
+            end--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+};

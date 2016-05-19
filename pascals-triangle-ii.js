ccffe1759aa560b9c4ad343139c17d4cb214e44e
gina0613 <sdi@usc.edu>
2016-05-18 18:30:18 -0700

    Pascals Triangle II

--- pascals-triangle-ii.js
+++ pascals-triangle-ii.js
@@ -3,24 +3,32 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    var f = [rowIndex][rowIndex];
+    var f = [[]];
 
     //Initialization
-    var i,j;
-    for(i=0;i<=rowIndex;i++) {
-        for(j=0;j<=rowIndex;j++) {
+    for(var i=0;i<=rowIndex;i++) {
+        for(var j=0;j<=rowIndex;j++) {
             if(j === 0 || i === j) {
+                // Javacript dont have the two dimensional array definition
+                // All it has is an array of an array
+                if (!f[i]) {
+                    f[i] = [];
+                }
                 f[i][j] = 1;
             }
         }
     }
 
     //Formula
-    for(var ii = 2; ii<=rowIndex ;ii++) {
-        for(var jj=2;jj<ii;jj++) {
+    for(var ii = 1; ii<=rowIndex ;ii++) {
+        for(var jj=1;jj<ii;jj++) {
             f[ii][jj] = f[ii-1][jj-1] + f[ii-1][jj];
         }
     }
 
-    return f[rowIndex][rowIndex];
+    return f[rowIndex];
 };
+
+/*
+* Pay attention to the way that javascript create two dimensional array;
+*/

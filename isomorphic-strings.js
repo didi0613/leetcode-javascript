7cb2a6905887a543364e71558ae4cef5bae8cf85
gina0613 <sdi@usc.edu>
2016-05-20 01:38:44 -0700

    Isomorphic strings

---
+++ isomorphic-strings.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    var len1 = s.length, len2 = t.length;
+
+    if(len1 !== len2) {
+        return false;
+    }
+
+    var mapA = [];
+    for(var i=0;i<len1;i++) {
+        if(s[i] in mapA) {
+            if(mapA[s[i]] !== t[i]) {
+                return false;
+            }
+        } else {
+            mapA[s[i]] = t[i];
+        }
+    }
+
+    var mapB = [];
+    for(var j=0;j<len1;j++) {
+        if(t[j] in mapB) {
+            if(mapB[t[j]] !== s[j]) {
+                return false;
+            }
+        } else {
+            mapB[t[j]] = s[j];
+        }
+    }
+
+    return true;
+};

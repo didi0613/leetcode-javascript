8e238a023952b0a33ce239c4f9db22676d91720d
gina0613 <sdi@usc.edu>
2016-05-20 13:49:15 -0700

    Valid Parenthesis

---
+++ valid-parentheses.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    var stack = [];
+
+    var len = s.length;
+    if(len===0) {
+        return true;
+    }
+    if(len ===1) {
+        return false;
+    }
+
+    for(var i=0;i<len;i++) {
+        switch(s[i]) {
+            case '(':
+                stack.push('(');
+                break;
+            case '{':
+                stack.push('{');
+                break;
+            case '[':
+                stack.push('[');
+                break;
+            case ')':
+                if(stack.pop() !== '(') {
+                    return false;
+                }
+                break;
+            case '}':
+                if(stack.pop() !== '{') {
+                    return false;
+                }
+                break;
+            case ']':
+                if(stack.pop() !== '[') {
+                    return false;
+                }
+                break;
+        }
+    }
+
+    return stack.length === 0;
+};

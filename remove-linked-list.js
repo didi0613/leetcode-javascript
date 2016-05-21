58cda0f6aca16bf790b1531e89efb1a778460e37
gina0613 <sdi@usc.edu>
2016-05-21 00:52:48 -0700

    Remove Linked List

---
+++ remove-linked-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+    if(head === null) {
+        return head;
+    }
+
+    if(head.val === val) {
+        return removeElements(head.next, val);
+    }
+
+    var prev = new ListNode(-1);
+    prev.next = head;
+    var cur = head;
+    while(cur !== null) {
+        if(cur.val === val) {
+            prev.next = cur.next;
+            cur = prev.next;
+        } else {
+            prev = cur;
+            cur = cur.next;
+        }
+    }
+
+    return head;
+};

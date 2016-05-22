992b9ea9c17f296268a18169fed32945f1b652df
gina0613 <sdi@usc.edu>
2016-05-22 00:01:19 -0700

    Palindrome Linked List

---
+++ palindrome-linked-list.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    var arr = [];
+    while(head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+
+    var len = arr.length;
+    if(len <= 1){
+        return true;
+    } else {
+        var start=0,end=len-1;
+        while(start<end) {
+            if(arr[start] !== arr[end]) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+};

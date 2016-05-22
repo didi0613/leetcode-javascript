0a328aa5b93cbb58bc615a0964872ce9840001d9
gina0613 <sdi@usc.edu>
2016-05-22 00:45:42 -0700

    Longest Common Prefix

---
+++ longest-common-prefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    var strnum = strs.length;
+
+    if(strnum === 0) {
+        return "";
+    } else if(strnum === 1) {
+        return strs[0];
+    }
+
+    var prefix = strs[0];
+
+    for(var i=1;i<strnum;i++) {
+        var len = Math.min(prefix.length, strs[i].length);
+        for(var j=0;j<len;j++) {
+            if(prefix[j] !== strs[i][j]) {
+                break;
+            }
+        }
+        prefix = prefix.substring(0,j);
+    }
+
+    return prefix;
+};

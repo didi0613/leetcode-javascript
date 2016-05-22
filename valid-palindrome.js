7f99c1a0add5a33dae87acbddb6d22fdacb1d5d1
gina0613 <sdi@usc.edu>
2016-05-22 10:50:55 -0700

    Valid Palindrome

---
+++ valid-palindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/\W/g,'').toLowerCase();
+
+    var arr = s.split('');
+    var len = arr.length;
+
+    if(len < 2) {
+        return true;
+    }
+    var start=0,end=len-1;
+    while(start<end) {
+        if(arr[start] !== arr[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+};

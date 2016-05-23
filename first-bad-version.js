713dd2627267a398df313b9a56da536f8bfd901a
gina0613 <sdi@usc.edu>
2016-05-23 15:38:41 -0700

    First bad version

---
+++ first-bad-version.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var low = 0,high=n;
+        while(low < high) {
+            var mid = (low+high)/2;
+            if(isBadVersion(mid)) {
+
+            }
+        }
+    };
+};

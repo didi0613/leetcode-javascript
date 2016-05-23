237828a02a67ac1b417f905adb1f13e807240eb1
gina0613 <sdi@usc.edu>
2016-05-23 12:22:55 -0700

    Reverse Integer

---
+++ reverse-integer.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    if(x<0) {
+        return -1 * reverse(-x);
+    }
+
+    var arr = x.toString().split('');
+    var len = arr.length;
+    if(len<=1) {
+        return x;
+    }
+
+    var start = 0, end = len-1;
+    while(start < end) {
+        var tmp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = tmp;
+        start++;
+        end--;
+    }
+
+    // Overflow check
+    if (parseInt(arr.join('')) >= 2147483648) {
+        return 0;
+    }
+
+    return parseInt(arr.join(''));
+};

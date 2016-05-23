56d3b8b6974dd257075d4a2fe8425c92d0d222b7
gina0613 <sdi@usc.edu>
2016-05-23 11:09:54 -0700

    Intersection of two arrays II

--- intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii.js
@@ -26,7 +26,7 @@ var intersect = function(nums1, nums2) {
 
     // Loop the longer array to check matches in hashmap
     for(var j=0;j<len1;j++) {
-        if(nums1[j] in hashmap && hashmap[nums1[j]] > 0) {
+        if(hashmap[nums1[j]] > 0) {
             ret.push(nums1[j]);
             hashmap[nums1[j]]--;
         }

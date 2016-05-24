290c41e8473fc835e00d5884456055682ab9d1f2
gina0613 <sdi@usc.edu>
2016-05-23 22:05:23 -0700

    Implement stack using queues

---
+++ implement-stack-using-queues.js
@@ -0,0 +1,35 @@
+/**
+ * @constructor
+ */
+var Stack = function() {
+    this.queue = [];
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Stack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @returns {void}
+ */
+Stack.prototype.pop = function() {
+    this.queue.pop();
+};
+
+/**
+ * @returns {number}
+ */
+Stack.prototype.top = function() {
+    return this.queue[this.queue.length-1];
+};
+
+/**
+ * @returns {boolean}
+ */
+Stack.prototype.empty = function() {
+    return this.queue.length === 0;
+};

51ed2aacad75152ca658ab219674bbbff235cb64
gina0613 <sdi@usc.edu>
2016-06-09 17:58:28 -0700

    Leetcode Javacript Solution

--- invert-binary-tree.js
+++ invert-binary-tree.js
@@ -9,8 +9,9 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-var invertTree = function(root) {
-    if(root) {
+// Solution 1
+var invertTree = function (root) {
+    if (root) {
         var temp = root.right;
         root.right = root.left;
         root.left = temp;
@@ -18,6 +19,33 @@ var invertTree = function(root) {
         invertTree(root.right);
         return root;
     } else {
+        return root;
+    }
+};
+
+//Solution 2
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+    if (root === null) {
+        return root;
+    } else if (root.left === null && root.right === null) {
+        return root
+    } else {
+        var temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
         return root;
     }
 };

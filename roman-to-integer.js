112af7b565c290ee16d68ba2b55af7fa1939a295
gina0613 <sdi@usc.edu>
2016-06-10 13:39:35 -0700

    Leetcode Javacript Easy Round 2

--- roman-to-integer.js
+++ roman-to-integer.js
@@ -2,49 +2,62 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    var dictionary = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+var romanToInt = function (s) {
+    var dictionary = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
     var s_array = s.split('');
     var len = s.length;
     var sum = 0;
 
-    for(var i = 0; i<len; i++) {
-        if(s[i] === 'C')
-        {
-            if(i === len-1) {
-                sum+=dictionary['C'];
-            } else if(s[i+1] === 'M') {
+    for (var i = 0; i < len; i++) {
+        if (s[i] === 'C') {
+            if (i === len - 1) {
+                sum += dictionary['C'];
+            } else if (s[i + 1] === 'M') {
                 sum += dictionary['CM'];
                 i++;
-            } else if(s[i+1] === 'D') {
+            } else if (s[i + 1] === 'D') {
                 sum += dictionary['CD'];
                 i++;
             } else {
-                sum+=dictionary['C'];
+                sum += dictionary['C'];
             }
-        } else if(s[i] === 'X') {
-            if(i === len-1) {
-                sum+=dictionary['X'];
-            } else if(s[i+1] === 'C') {
+        } else if (s[i] === 'X') {
+            if (i === len - 1) {
+                sum += dictionary['X'];
+            } else if (s[i + 1] === 'C') {
                 sum += dictionary['XC'];
                 i++;
-            } else if(s[i+1] === 'L') {
+            } else if (s[i + 1] === 'L') {
                 sum += dictionary['XL'];
                 i++;
             } else {
-                sum+=dictionary['X'];
+                sum += dictionary['X'];
             }
-        } else if(s[i] === 'I') {
-            if(i === len-1) {
-                sum+=dictionary['I'];
-            } else if(s[i+1] === 'X') {
+        } else if (s[i] === 'I') {
+            if (i === len - 1) {
+                sum += dictionary['I'];
+            } else if (s[i + 1] === 'X') {
                 sum += dictionary['IX'];
                 i++;
-            } else if(s[i+1] === 'V') {
+            } else if (s[i + 1] === 'V') {
                 sum += dictionary['IV'];
                 i++;
             } else {
-                sum+=dictionary['I'];
+                sum += dictionary['I'];
             }
         } else {
             sum += dictionary[s[i]];

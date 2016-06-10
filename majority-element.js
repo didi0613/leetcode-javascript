fc3b3849e8b7f3292a5a76f8985b9d6a699fe16c
gina0613 <sdi@usc.edu>
2016-06-10 10:00:47 -0700

    Leetcode Javacript Easy Round 2

--- majority-element.js
+++ majority-element.js
@@ -2,17 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
     var len = nums.length;
     var ret = nums[0];
     var counter = 1, i = 0;
 
     nums.sort();
 
-    while(i < len-1) {
-        if(nums[i] === nums[i+1]) {
+    while (i < len - 1) {
+        if (nums[i] === nums[i + 1]) {
             counter++;
-            if(counter > len/2) {
+            if (counter > len / 2) {
                 ret = nums[i];
                 break;
             }

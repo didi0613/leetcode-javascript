5e1a235288dfe7300f2ddf4e46b5239c5f81acbc
gina0613 <sdi@usc.edu>
2016-06-10 14:00:01 -0700

    Leetcode Javacript Easy Round 2

--- lowest-common-ancestor-of-a-binary-search-tree.js
+++ lowest-common-ancestor-of-a-binary-search-tree.js
@@ -11,6 +11,7 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
+// Solution 1
 var lowestCommonAncestor = function (root, p, q) {
     if (root == null || root === p || root === q) {
         return root;
@@ -28,5 +29,29 @@ var lowestCommonAncestor = function (root, p, q) {
         } else {
             return lowestCommonAncestor(root.left, p, q);
         }
+    }
+};
+
+// Solution 2
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if ((root.val - p.val) * (root.val - q.val) <= 0) {
+        return root;
+    } else if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    } else if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
     }
 };

3e06b616d0107c565708198df2292409b208aab7
gina0613 <sdi@usc.edu>
2016-06-10 13:50:25 -0700

    Leetcode Javacript Easy Round 2

--- power-of-two.js
+++ power-of-two.js
@@ -2,18 +2,40 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfTwo = function(n) {
-    if(n<=0) {
+// Solution 1
+var isPowerOfTwo = function (n) {
+    if (n <= 0) {
         return false;
     }
 
-    while(n%2 === 0) {
-        n = n/2;
+    while (n % 2 === 0) {
+        n = n / 2;
     }
 
-    if(n>1) {
+    if (n > 1) {
         return false;
     } else {
         return true;
     }
+};
+
+// Solution 2
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    while (n > 1) {
+        if (n % 2 !== 0) {
+            return false;
+        } else {
+            n = n / 2;
+        }
+    }
+
+    return true;
 };

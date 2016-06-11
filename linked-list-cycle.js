4b70c668fc6d20c3088b223812a3b9e425e65a62
gina0613 <sdi@usc.edu>
2016-06-11 11:02:48 -0700

    Leetcode Javacript Easy Round 2

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -10,19 +10,20 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    if(head === null || head.next === null) {
+// Solution 1
+var hasCycle = function (head) {
+    if (head === null || head.next === null) {
         return false;
     }
 
     var slow = head;
     var fast = head.next.next;
 
-    while(slow && fast) {
-        if(slow === fast) {
+    while (slow && fast) {
+        if (slow === fast) {
             return true;
         } else {
-            if(slow.next && fast.next && fast.next.next) {
+            if (slow.next && fast.next && fast.next.next) {
                 slow = slow.next;
                 fast = fast.next.next;
             } else {
@@ -31,5 +32,32 @@ var hasCycle = function(head) {
         }
     }
 
+    return false;
+};
+
+// Solution 2
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    var fast = head;
+    var slow = head;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+
+        if (fast === slow) {
+            return true;
+        }
+    }
     return false;
 };

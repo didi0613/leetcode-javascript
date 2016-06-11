c0cf86389612688a4780cd91d1d5ad0446e423ca
gina0613 <sdi@usc.edu>
2016-06-10 19:24:23 -0700

    Leetcode Javacript Easy Round 2

--- number-of-1-bits.js
+++ number-of-1-bits.js
@@ -36,3 +36,17 @@ var hammingWeight = function (n) {
     }
     return ret;
 };
+
+// Solution 3
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+    var ret = 0;
+    while (n !== 0) {
+        n &= (n - 1);
+        ret++;
+    }
+    return ret;
+};

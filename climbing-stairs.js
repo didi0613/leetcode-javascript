341f5b561bc05e34dffcb9c9a0d89ded7d94f7da
gina0613 <sdi@usc.edu>
2016-06-10 22:37:57 -0700

    Leetcode Javacript Easy Round 2

--- climbing-stairs.js
+++ climbing-stairs.js
@@ -2,14 +2,14 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+var climbStairs = function (n) {
     var ret = [];
     ret[0] = 0;
     ret[1] = 1;
     ret[2] = 2;
 
-    for(var i=3;i<=n;i++) {
-        ret[i] = ret[i-1] + ret[i-2];
+    for (var i = 3; i <= n; i++) {
+        ret[i] = ret[i - 1] + ret[i - 2];
     }
 
     return ret[n];

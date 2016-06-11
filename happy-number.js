76841ab9d87a162590a612503ee1ba5151e040cc
gina0613 <sdi@usc.edu>
2016-06-10 23:08:49 -0700

    Leetcode Javacript Easy Round 2

--- happy-number.js
+++ happy-number.js
@@ -2,6 +2,7 @@
  * @param {number} n
  * @return {boolean}
  */
+// Solution 1
 var isHappy = function (n) {
     if (n === 1 || n === 7) {
         return true;
@@ -30,4 +31,32 @@ var isHappy = function (n) {
             return isHappy(ret);
         }
     }
+};
+
+// Solution 2
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+function isHappyHelper(n, hash) {
+    var arr = n.toString().split('');
+    var ret = 0;
+    for (var i in arr) {
+        ret += parseInt(arr[i]) * parseInt(arr[i]);
+    }
+
+    if (ret === 1) {
+        return true;
+    } else {
+        if (hash.includes(ret)) {
+            return false;
+        } else {
+            hash.push(ret);
+            return isHappyHelper(ret, hash);
+        }
+    }
+}
+
+var isHappy = function (n) {
+    return isHappyHelper(n, []);
 };

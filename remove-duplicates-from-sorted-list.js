afad86c50daa4ecf7ebba0ac9bd156f53256a78d
gina0613 <sdi@usc.edu>
2016-06-10 22:29:21 -0700

    Leetcode Javacript Easy Round 2

--- remove-duplicates-from-sorted-list.js
+++ remove-duplicates-from-sorted-list.js
@@ -9,21 +9,57 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
+// Solution 1
+var deleteDuplicates = function (head) {
     var cur = head;
-    while(cur) {
+    while (cur) {
         var runner = cur.next;
-        while(runner && runner.val === cur.val) {
+        while (runner && runner.val === cur.val) {
             runner = runner.next;
             cur.next = runner;
         }
 
-        if(runner === null) {
+        if (runner === null) {
             return head;
         } else {
             cur.next = runner;
             cur = runner;
         }
+    }
+
+    return head;
+};
+
+// Solution 2
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (head === null || head.next === null) {
+        return head;
+    }
+
+    var node = head;
+    var last = node.val;
+    while (node.next !== null) {
+        if (node.next.val === last) {
+            node.next = node.next.next;
+        } else {
+            last = node.next.val;
+            node = node.next;
+        }
+    }
+
+    if (node.val === last) {
+        node = null;
     }
 
     return head;

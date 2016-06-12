c158d14f08b2386fb7b840818d0dc9cf34397432
gina0613 <sdi@usc.edu>
2016-06-12 11:11:40 -0700

    Leetcode Javacript Easy Round 2

--- balanced-binary-tree.js
+++ balanced-binary-tree.js
@@ -11,19 +11,19 @@
  */
 
 function maxHeight(node) {
-    if(node === null) {
+    if (node === null) {
         return 0;
     }
 
-    if(node.left === null && node.right===null) {
+    if (node.left === null && node.right === null) {
         return 1;
     }
 
     return Math.max(maxHeight(node.left), maxHeight(node.right)) + 1;
 }
 
-var isBalanced = function(root) {
-    if(root === null || (root.left === null && root.right === null)) {
+var isBalanced = function (root) {
+    if (root === null || (root.left === null && root.right === null)) {
         return true;
     }
 

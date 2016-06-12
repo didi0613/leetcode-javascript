f3d0589536736d26b471f7e0144b01403e7f2f9c
gina0613 <sdi@usc.edu>
2016-06-12 10:40:22 -0700

    Leetcode Javacript Easy Round 2

--- house-robber.js
+++ house-robber.js
@@ -2,12 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
+// Solution 1
+var rob = function (nums) {
     var ret = 0;
     var len = nums.length;
-    if(len === 0) {
+    if (len === 0) {
         return ret;
-    } else if(len === 1) {
+    } else if (len === 1) {
         return nums[0];
     }
 
@@ -16,10 +17,31 @@ var rob = function(nums) {
     f[1] = Math.max(nums[0], nums[1]);
     ret = Math.max(f[0], f[1]);
 
-    for(var i = 2; i < len; i++) {
-        f[i] = Math.max(ret, f[i-2]+nums[i]);
+    for (var i = 2; i < len; i++) {
+        f[i] = Math.max(ret, f[i - 2] + nums[i]);
         ret = Math.max(ret, f[i]);
     }
 
-    return f[len-1];
+    return f[len - 1];
+};
+
+// Solution 2
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    var len = nums.length;
+    if (len === 0) {
+        return 0;
+    }
+
+    var f = [];
+    f[0] = nums[0];
+    f[1] = Math.max(nums[0], nums[1]);
+    for (var i = 2; i < len; i++) {
+        f[i] = Math.max(f[i - 2] + nums[i], f[i - 1]);
+    }
+
+    return f[len - 1];
 };

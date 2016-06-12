3a63082855e60673cc7cba7630ac4383f3c837f2
gina0613 <sdi@usc.edu>
2016-06-12 13:27:00 -0700

    Leetcode Javacript Easy Round 2

--- binary-tree-level-order-traversal-ii.js
+++ binary-tree-level-order-traversal-ii.js
@@ -12,30 +12,29 @@
 var levelOrderBottom = function (root) {
     if (root === null) {
         return [];
-    } else {
-        var lastLevel = [];
-        var ret = [];
-        var lastLevelData;
-
-        lastLevel.push(root);
-
-        while (lastLevel.length > 0) {
-            lastLevelData = [];
-            var level = [];
-
-            for (var i in lastLevel) {
-                if (lastLevel[i].left) {
-                    level.push(lastLevel[i].left);
-                }
-                if (lastLevel[i].right) {
-                    level.push(lastLevel[i].right);
-                }
-                lastLevelData.push(lastLevel[i].val);
-            }
-            ret.unshift(lastLevelData);
-            lastLevel = level;
-        }
-
-        return ret;
     }
+
+    var ret = [];
+    var lastLevel = [root];
+
+    while (lastLevel.length > 0) {
+        var lastLevelData = [];
+        var currentLevel = [];
+
+        for (var i in lastLevel) {
+            lastLevelData.push(lastLevel[i].val);
+
+            if (lastLevel[i].left !== null) {
+                currentLevel.push(lastLevel[i].left);
+            }
+
+            if (lastLevel[i].right !== null) {
+                currentLevel.push(lastLevel[i].right);
+            }
+        }
+        ret.unshift(lastLevelData);
+        lastLevel = currentLevel;
+    }
+
+    return ret;
 };

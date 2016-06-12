aa8185b85029970bf815b36a8b8094799bee98fe
gina0613 <sdi@usc.edu>
2016-06-12 09:55:11 -0700

    Leetcode Javacript Easy Round 2

--- swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs.js
@@ -10,7 +10,7 @@
  * @return {ListNode}
  */
 
-function swapnode(h,r1,r2){
+function swapnode(h, r1, r2) {
     var next = r2.next;
     h.next = r2;
     r2.next = r1;
@@ -19,13 +19,13 @@ function swapnode(h,r1,r2){
     return r1;
 }
 
-var swapPairs = function(head) {
+var swapPairs = function (head) {
     var newhead = new ListNode(-1);
     newhead.next = head;
     start = newhead;
 
-    while(start.next && start.next.next) {
-        start = swapnode(start,start.next,start.next.next);
+    while (start.next && start.next.next) {
+        start = swapnode(start, start.next, start.next.next);
     }
 
 
@@ -33,9 +33,9 @@ var swapPairs = function(head) {
 };
 
 /* Summarize
-* Dummy Node 可以认为是一个假的头结点。
-* 我们可以构造一个Dummy Node ，使他的next指针指向Head node。
-* 使用Dummy Node目的是，在单向链表中，保正head不会再删除操作中丢失。
-* 此外，比较特殊的方法是用来进行删除head。
-* Dummy Node可以处理对于head Node变化的情况。
-*/
+ * Dummy Node 可以认为是一个假的头结点。
+ * 我们可以构造一个Dummy Node ，使他的next指针指向Head node。
+ * 使用Dummy Node目的是，在单向链表中，保正head不会再删除操作中丢失。
+ * 此外，比较特殊的方法是用来进行删除head。
+ * Dummy Node可以处理对于head Node变化的情况。
+ */

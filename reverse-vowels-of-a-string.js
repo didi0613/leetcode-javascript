107211b858c392a1f9b885be6f1ef13d04129282
gina0613 <sdi@usc.edu>
2016-06-12 10:19:14 -0700

    Leetcode Javacript Easy Round 2

--- reverse-vowels-of-a-string.js
+++ reverse-vowels-of-a-string.js
@@ -2,22 +2,22 @@
  * @param {string} s
  * @return {string}
  */
-var reverseVowels = function(s) {
+var reverseVowels = function (s) {
     var len = s.length;
-    var start = 0, end = len-1;
-    var vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    var start = 0, end = len - 1;
+    var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     var sarray = s.split('');
 
-    while(start < end) {
-        while(start < len && vowels.indexOf(sarray[start]) === -1) {
+    while (start < end) {
+        while (start < len && vowels.indexOf(sarray[start]) === -1) {
             start++;
         }
 
-        while(end >=0 && vowels.indexOf(sarray[end]) === -1) {
+        while (end >= 0 && vowels.indexOf(sarray[end]) === -1) {
             end--;
         }
 
-        if(start < end) {
+        if (start < end) {
             var temp = sarray[end];
             sarray[end] = sarray[start];
             sarray[start] = temp;

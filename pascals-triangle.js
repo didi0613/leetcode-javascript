5834fb27558fc514592972c030876381886ef411
gina0613 <sdi@usc.edu>
2016-06-13 13:26:09 -0700

    Leetcode Easy Round 2

--- pascals-triangle.js
+++ pascals-triangle.js
@@ -2,30 +2,30 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {
+var generate = function (numRows) {
     // dp[n][m] = 1 (dp[n][0] || dp[n][n])
     //          = dp[n-1][m-1] + dp[n-1][m]
 
     var ret = [];
-    if(numRows === 0) {
+    if (numRows === 0) {
         return ret;
     }
 
-    if(numRows === 1) {
+    if (numRows === 1) {
         return [[1]];
     }
 
-    var lastline = [1,1];
+    var lastline = [1, 1];
     ret.push([1]);
     ret.push(lastline);
 
-    for(var i=3;i<=numRows;i++) {
+    for (var i = 3; i <= numRows; i++) {
         var line = [];
-        for(var j=0;j<i;j++) {
-            if(j ===0 || j === i-1) {
+        for (var j = 0; j < i; j++) {
+            if (j === 0 || j === i - 1) {
                 line.push(1)
             } else {
-                line.push(lastline[j-1]+lastline[j]);
+                line.push(lastline[j - 1] + lastline[j]);
             }
         }
         lastline = line;

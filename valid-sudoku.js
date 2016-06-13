794f9d2749ac989cfc26eb353a5100a077ab8b1d
gina0613 <sdi@usc.edu>
2016-06-13 16:50:59 -0700

    Leetcode Easy Round 2

--- valid-sudoku.js
+++ valid-sudoku.js
@@ -3,12 +3,12 @@
  * @return {boolean}
  */
 
-function ValidRow(i,board) {
+function ValidRow(i, board) {
     var hashmap = [];
 
-    for(var p=0;p<9;p++) {
-        if(board[i][p] !== '.') {
-            if(hashmap.includes(board[i][p])) {
+    for (var p = 0; p < 9; p++) {
+        if (board[i][p] !== '.') {
+            if (hashmap.includes(board[i][p])) {
                 return false;
             } else {
                 hashmap.push(board[i][p])
@@ -18,12 +18,12 @@ function ValidRow(i,board) {
     return true;
 }
 
-function ValidCol(i,board) {
+function ValidCol(i, board) {
     var hashMap = [];
 
-    for(var q=0;q<9;q++) {
-        if(board[q][i] !== '.') {
-            if(hashMap.includes(board[q][i])) {
+    for (var q = 0; q < 9; q++) {
+        if (board[q][i] !== '.') {
+            if (hashMap.includes(board[q][i])) {
                 return false;
             } else {
                 hashMap.push(board[q][i]);
@@ -33,13 +33,13 @@ function ValidCol(i,board) {
     return true;
 }
 
-function ValidSquare(row,col,board) {
+function ValidSquare(row, col, board) {
     var history = [];
 
-    for(var m=row*3; m<(row+1)*3;m++) {
-        for(var n=col*3;n<(col+1)*3;n++) {
-            if(board[m][n] !== '.') {
-                if(history.includes(board[m][n])) {
+    for (var m = row * 3; m < (row + 1) * 3; m++) {
+        for (var n = col * 3; n < (col + 1) * 3; n++) {
+            if (board[m][n] !== '.') {
+                if (history.includes(board[m][n])) {
                     return false;
                 } else {
                     history.push(board[m][n]);
@@ -50,16 +50,16 @@ function ValidSquare(row,col,board) {
     return true;
 }
 
-var isValidSudoku = function(board) {
-    for(var i=0;i<9;i++) {
-        if(!(ValidRow(i, board) && ValidCol(i, board))) {
+var isValidSudoku = function (board) {
+    for (var i = 0; i < 9; i++) {
+        if (!(ValidRow(i, board) && ValidCol(i, board))) {
             return false;
         }
     }
 
-    for(var j=0;j<3;j++) {
-        for(var jj=0;jj<3;jj++) {
-            if(!ValidSquare(j,jj, board)) {
+    for (var j = 0; j < 3; j++) {
+        for (var jj = 0; jj < 3; jj++) {
+            if (!ValidSquare(j, jj, board)) {
                 return false;
             }
         }

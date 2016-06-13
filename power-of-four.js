1ece50184b7c776d96c10721dc072f56c94bae57
gina0613 <sdi@usc.edu>
2016-06-13 13:21:09 -0700

    Leetcode Easy Round 2

--- power-of-four.js
+++ power-of-four.js
@@ -2,6 +2,7 @@
  * @param {number} num
  * @return {boolean}
  */
+// Solution 1
 var isPowerOfFour = function (num) {
     while (num > 1) {
         if (num % 4 === 0) {
@@ -25,3 +26,20 @@ var isPowerOfFour = function (num) {
  * A 4 byte int range:
  * -2,147,483,648 to 2,147,483,647
  */
+
+// Solution 2
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function (num) {
+    if (num <= 0) {
+        return false;
+    }
+    if (num === 1) {
+        return true;
+    }
+
+    var log100 = Math.log(parseInt(num.toString(2))) / Math.log(100);
+    return Math.abs(Math.round(log100) - log100) < 1e-10 ? true : false;
+};

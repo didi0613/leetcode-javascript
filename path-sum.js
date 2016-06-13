338e79295a225928de8be24ab6a5ae144aa6167d
gina0613 <sdi@usc.edu>
2016-06-13 16:25:29 -0700

    Leetcode Easy Round 2

--- path-sum.js
+++ path-sum.js
@@ -10,14 +10,15 @@
  * @param {number} sum
  * @return {boolean}
  */
-var hasPathSum = function(root, sum) {
-    if(root === null) {
+var hasPathSum = function (root, sum) {
+    if (root === null) {
         return false;
     }
 
-    if(root.val === sum && !root.left && !root.right) {
+    // DFS
+    if (root.val === sum && root.left === null && root.right === null) {
         return true;
+    } else {
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
-
-    return hasPathSum(root.left, sum-root.val) || hasPathSum(root.right, sum-root.val);
 };

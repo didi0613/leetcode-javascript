9c029a6731338723fd522ba5484cc2849dcd47d0
gina0613 <sdi@usc.edu>
2016-06-13 14:24:44 -0700

    Leetcode Easy Round 2

--- binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal.js
@@ -9,23 +9,23 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-    if(root === null) {
+var levelOrder = function (root) {
+    if (root === null) {
         return [];
     }
 
     var lastLevel = [root];
     var ret = [];
 
-    while(lastLevel.length > 0) {
+    while (lastLevel.length > 0) {
         var levelData = [];
         var level = [];
-        for(var i=0;i<lastLevel.length;i++) {
-            if(lastLevel[i].left) {
+        for (var i = 0; i < lastLevel.length; i++) {
+            if (lastLevel[i].left) {
                 level.push(lastLevel[i].left);
             }
 
-            if(lastLevel[i].right) {
+            if (lastLevel[i].right) {
                 level.push(lastLevel[i].right);
             }
             levelData.push(lastLevel[i].val);
@@ -33,6 +33,52 @@ var levelOrder = function(root) {
 
         ret.push(levelData);
         lastLevel = level;
+    }
+
+    return ret;
+};
+
+// Second try
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (root === null) {
+        return [];
+    }
+
+    if (root.left === null && root.right === null) {
+        return [root];
+    }
+
+    var ret = [];
+    var lastLevel = [root];
+    while (lastLevel.length > 0) {
+        var currentLevel = [];
+        var lastLevelData = [];
+
+        for (var i in lastLevel) {
+            lastLevelData.push(lastLevel[i].val);
+
+            if (lastLevel[i].left) {
+                currentLevel.push(lastLevel[i].left);
+            }
+
+            if (lastLevel[i].right) {
+                currentLevel.push(lastLevel[i].right);
+            }
+        }
+
+        ret.push(lastLevelData);
+        lastLevel = currentLevel;
     }
 
     return ret;

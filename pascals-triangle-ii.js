c8967d7bf33d0fbbf9cc47dc9333fb525da0730e
gina0613 <sdi@usc.edu>
2016-06-13 15:34:01 -0700

    Leetcode Easy Round 2

--- pascals-triangle-ii.js
+++ pascals-triangle-ii.js
@@ -2,6 +2,7 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
+// Solution 1
 var getRow = function (rowIndex) {
     var f = [[]];
 
@@ -32,3 +33,28 @@ var getRow = function (rowIndex) {
 /*
  * Pay attention to the way that javascript create two dimensional array;
  */
+
+// Solution 2
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function (rowIndex) {
+    if (rowIndex === 0) {
+        return [1];
+    }
+
+    var lastLevel = [1];
+    while (rowIndex > 0) {
+        var currentLevel = [];
+        currentLevel.push(1);
+        for (var i = 0; i < lastLevel.length - 1; i++) {
+            currentLevel.push(lastLevel[i] + lastLevel[i + 1]);
+        }
+        currentLevel.push(1);
+        rowIndex--;
+        lastLevel = currentLevel;
+    }
+
+    return lastLevel;
+};

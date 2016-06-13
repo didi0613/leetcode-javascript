65e947dc691ff51596f1541383933df755f3ff91
gina0613 <sdi@usc.edu>
2016-06-13 12:39:13 -0700

    Leetcode Easy Round 2

--- plus-one.js
+++ plus-one.js
@@ -2,21 +2,21 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
+var plusOne = function (digits) {
     var len = digits.length, carry = 1;
     var ret = [];
 
-    if(len === 0) {
+    if (len === 0) {
         return ret;
     }
 
-    for(var i=len-1;i>=0;i--) {
+    for (var i = len - 1; i >= 0; i--) {
         var sum = digits[i] + carry;
-        ret.unshift(sum%10);
-        carry = parseInt(sum/10);
+        ret.unshift(sum % 10);
+        carry = parseInt(sum / 10);
     }
 
-    if(carry === 1) {
+    if (carry === 1) {
         ret.unshift(carry);
     }
 

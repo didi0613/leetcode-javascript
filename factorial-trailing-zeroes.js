f78c7b3a64cf62aa0a8eb982170ffcdd69fd85e3
gina0613 <sdi@usc.edu>
2016-06-13 22:06:08 -0700

    Leetcode Easy Round 2

--- factorial-trailing-zeroes.js
+++ factorial-trailing-zeroes.js
@@ -3,19 +3,13 @@
  * @return {number}
  */
 var trailingZeroes = function (n) {
-    // Divider 5,5*5,5*5*5...
     var divider = 5;
     var ret = 0;
-    while (divider <= n) {
+
+    while (n >= divider) {
         ret += Math.floor(n / divider);
         divider = divider * 5;
     }
 
     return ret;
 };
-
-/*
- * Summarize:
- * n!后缀0的个数 = n!质因子中5的个数
- = floor(n/5) + floor(n/25) + floor(n/125) + ....
- */

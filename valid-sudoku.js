a839757a04c00588549f41b076b8e01a25a637be
gina0613 <sdi@usc.edu>
2016-06-13 23:59:04 -0700

    Leetcode Easy Round 2

--- valid-sudoku.js
+++ valid-sudoku.js
@@ -2,7 +2,7 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-
+// Solution 1
 function ValidRow(i, board) {
     var hashmap = [];
 
@@ -61,6 +61,56 @@ var isValidSudoku = function (board) {
         for (var jj = 0; jj < 3; jj++) {
             if (!ValidSquare(j, jj, board)) {
                 return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+// Solution 2
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+
+var isValidSudoku = function (board) {
+    for (i = 0; i < 9; i++) {
+        var hashRow = [];
+        var hashCol = [];
+        for (j = 0; j < 9; j++) {
+            if (board[i][j] !== '.') {
+                if (hashRow.includes(board[i][j])) {
+                    return false;
+                } else {
+                    hashRow.push(board[i][j]);
+                }
+            }
+
+            if (board[j][i] !== '.') {
+                if (hashCol.includes(board[j][i])) {
+                    return false;
+                } else {
+                    hashCol.push(board[j][i]);
+                }
+            }
+        }
+    }
+
+    // check square
+    for (var p = 0; p < 9; p += 3) {
+        for (var q = 0; q < 9; q += 3) {
+            var hashSquare = [];
+            for (var a = 0; a < 3; a++) {
+                for (var b = 0; b < 3; b++) {
+                    if (board[a + p][b + q] !== '.') {
+                        if (hashSquare.includes(board[a + p][b + q])) {
+                            return false;
+                        } else {
+                            hashSquare.push(board[a + p][b + q]);
+                        }
+                    }
+                }
             }
         }
     }

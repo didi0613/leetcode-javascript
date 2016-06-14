eb173353dbca167918c6665783db92efbf103f62
gina0613 <sdi@usc.edu>
2016-06-14 14:41:40 -0700

    Leetcode Easy Round 2

--- longest-common-prefix.js
+++ longest-common-prefix.js
@@ -2,25 +2,25 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
+var longestCommonPrefix = function (strs) {
     var strnum = strs.length;
 
-    if(strnum === 0) {
+    if (strnum === 0) {
         return "";
-    } else if(strnum === 1) {
+    } else if (strnum === 1) {
         return strs[0];
     }
 
     var prefix = strs[0];
 
-    for(var i=1;i<strnum;i++) {
+    for (var i = 1; i < strnum; i++) {
         var len = Math.min(prefix.length, strs[i].length);
-        for(var j=0;j<len;j++) {
-            if(prefix[j] !== strs[i][j]) {
+        for (var j = 0; j < len; j++) {
+            if (prefix[j] !== strs[i][j]) {
                 break;
             }
         }
-        prefix = prefix.substring(0,j);
+        prefix = prefix.substring(0, j);
     }
 
     return prefix;

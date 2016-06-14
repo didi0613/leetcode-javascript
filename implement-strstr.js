da22dbc1ed6ee2691943bc2cff89432663eebb0a
gina0613 <sdi@usc.edu>
2016-06-14 15:53:45 -0700

    Leetcode Easy Round 2

--- implement-strstr.js
+++ implement-strstr.js
@@ -1,3 +1,4 @@
+// Solution 1
 /**
  * @param {string} haystack
  * @param {string} needle
@@ -5,4 +6,47 @@
  */
 var strStr = function (haystack, needle) {
     return haystack.indexOf(needle);
+};
+
+// Solution 2
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    if (haystack === "") {
+        return needle === "" ? 0 : -1;
+    }
+
+    if (needle === "") {
+        return 0;
+    }
+
+    //Guarantee haystack non-empty
+    var len1 = haystack.length;
+    var len2 = needle.length;
+
+    if (len1 < len2) {
+        return -1;
+    } else if (len1 === len2) {
+        return haystack === needle ? 0 : -1;
+    } else {
+        //guarantee haystack is longer than needle
+        var startpoint = 0;
+        while (startpoint <= len1 - len2) {
+            var parent = startpoint, child = 0;
+            while (haystack[parent] === needle[child]) {
+                parent++;
+                child++;
+
+                if (child === len2) {
+                    return startpoint;
+                }
+            }
+            startpoint++;
+        }
+
+        return -1;
+    }
 };

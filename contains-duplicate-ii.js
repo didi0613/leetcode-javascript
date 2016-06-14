7ee97b8388ca21e16499c38b467f566b6fbdfba0
gina0613 <sdi@usc.edu>
2016-06-13 17:08:20 -0700

    Leetcode Easy Round 2

--- contains-duplicate-ii.js
+++ contains-duplicate-ii.js
@@ -3,12 +3,12 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function(nums, k) {
+var containsNearbyDuplicate = function (nums, k) {
     var len = nums.length;
 
-    for(var i=0;i<len-1;i++) {
-        for(var j=len-1;j>i;j--) {
-            if(nums[i] === nums[j] && (j-i)<=k) {
+    for (var i = 0; i < len - 1; i++) {
+        for (var j = len - 1; j > i; j--) {
+            if (nums[i] === nums[j] && (j - i) <= k) {
                 return true;
             }
         }

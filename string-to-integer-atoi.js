c4470335e7c681056f9fd211cf168ba86d3ca69f
gina0613 <sdi@usc.edu>
2016-06-14 08:52:36 -0700

    Leetcode Easy Round 2

--- string-to-integer-atoi.js
+++ string-to-integer-atoi.js
@@ -2,18 +2,16 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function(str) {
-    var res = parseInt(str);
-      if(res > 2147483647){
-          res = 2147483647;
-      }else if(res < -2147483648){
-         res = -2147483648;
-     }
-     return isNaN(res) ? 0 : res;
+var myAtoi = function (str) {
+    if (Number.isInteger(parseInt(str))) {
+        if (parseInt(str) > Math.pow(2, 31) - 1) {
+            return Math.pow(2, 31) - 1;
+        } else if (parseInt(str) < Math.pow(2, 31) * (-1)) {
+            return Math.pow(2, 31) * (-1)
+        } else {
+            return parseInt(str);
+        }
+    } else {
+        return 0;
+    }
 };
-
-/*
-* Summarize:
-* maximum value of a 32 bit Integer: 2147483647
-* minimum value of a 32 bit Integer: -2147483648
-*/

2369228bd846de646847abb7adbfe7132d852a7e
gina0613 <sdi@usc.edu>
2016-06-15 12:17:01 -0700

    Leetcode Easy Round 2

--- palindrome-linked-list.js
+++ palindrome-linked-list.js
@@ -40,3 +40,52 @@ var isPalindrome = function (head) {
 // Solution 2
 // Space Complexity O(1)
 // Time Complexity O(N)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    if (head === null || head.next === null) {
+        return true;
+    }
+
+    // Find the mid of the linked list
+    var slow = head;
+    var fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // reverse the second half of the list
+    var node = slow;
+    var prev = null;
+    while (node.next) {
+        var next = node.next;
+        node.next = prev;
+
+        prev = node;
+        node = next;
+    }
+    node.next = prev;
+
+    // compare head and node
+    var runner1 = head, runner2 = node;
+    while (runner1 && runner2) {
+        if (runner1.val !== runner2.val) {
+            return false;
+        } else {
+            runner1 = runner1.next;
+            runner2 = runner2.next;
+        }
+    }
+
+    return true;
+};

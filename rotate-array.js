e350910c0b909da8aabdbc47917933d0d0f28250
gina0613 <sdi@usc.edu>
2016-06-14 23:00:10 -0700

    Leetcode Easy Round 2

--- rotate-array.js
+++ rotate-array.js
@@ -3,11 +3,11 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function(nums, k) {
-    k = k%nums.length;
+var rotate = function (nums, k) {
+    k = k % nums.length;
     // get the last k digits of nums
-    var tmp = nums.splice(-k,k);
-    for(var i=k-1;i>=0;i--) {
+    var tmp = nums.splice(-k, k);
+    for (var i = k - 1; i >= 0; i--) {
         nums.unshift(tmp[i]);
     }
 };

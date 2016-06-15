c43cb27464d827e847f8682d07648c1f3a828f8d
gina0613 <sdi@usc.edu>
2016-06-15 16:46:41 -0700

    Leetcode Easy Round 2

--- count-and-say.js
+++ count-and-say.js
@@ -5,7 +5,7 @@
 
 function read(str) {
     var len = str.length;
-    if(len === 1) {
+    if (len === 1) {
         return "1" + str[0];
     }
 
@@ -13,35 +13,35 @@ function read(str) {
     var target = str[0];
     var ret = "";
 
-    for(var k=1;k<len;k++) {
-        while(str[k] === target) {
+    for (var k = 1; k < len; k++) {
+        while (str[k] === target) {
             count++;
             k++;
         }
-        ret+=count+str[k-1];
-        if(k>=len) {
+        ret += count + str[k - 1];
+        if (k >= len) {
             return ret;
         } else {
-            count=1;
-            target= str[k];
+            count = 1;
+            target = str[k];
         }
     }
 
-    ret+=count+str[len-1];
+    ret += count + str[len - 1];
 
     return ret;
 }
 
-var countAndSay = function(n) {
-    if(n<=0) {
+var countAndSay = function (n) {
+    if (n <= 0) {
         return "";
     }
 
     var f = [];
     f[0] = "1";
-    for(var i=1;i<n;i++) {
-        f[i] = read(f[i-1]);
+    for (var i = 1; i < n; i++) {
+        f[i] = read(f[i - 1]);
     }
 
-    return f[n-1];
+    return f[n - 1];
 };

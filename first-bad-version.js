1b815a508c82641aa3de4279602d936f30fbc858
gina0613 <sdi@usc.edu>
2016-06-14 19:00:34 -0700

    Leetcode Easy Round 2

--- first-bad-version.js
+++ first-bad-version.js
@@ -12,24 +12,24 @@
  * @param {function} isBadVersion()
  * @return {function}
  */
-var solution = function(isBadVersion) {
+var solution = function (isBadVersion) {
     /**
      * @param {integer} n Total versions
      * @return {integer} The first bad version
      */
-    return function(n) {
-        return findBadVersion(1,n);
+    return function (n) {
+        return findBadVersion(1, n);
 
-        function findBadVersion(start,end) {
-            var mid = parseInt((start+end)/2);
-            if(isBadVersion(mid)) {
-                if(!isBadVersion(mid-1)) {
+        function findBadVersion(start, end) {
+            var mid = parseInt((start + end) / 2);
+            if (isBadVersion(mid)) {
+                if (!isBadVersion(mid - 1)) {
                     return mid;
                 } else {
-                    return findBadVersion(start,mid-1);
+                    return findBadVersion(start, mid - 1);
                 }
             } else {
-                return findBadVersion(mid+1,end);
+                return findBadVersion(mid + 1, end);
             }
         }
     };

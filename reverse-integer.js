fa684ec7eb54988081906491672efb9c25e00535
gina0613 <sdi@usc.edu>
2016-06-14 18:55:33 -0700

    Leetcode Easy Round 2

--- reverse-integer.js
+++ reverse-integer.js
@@ -1,20 +1,21 @@
+// Solution 1
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    if(x<0) {
+var reverse = function (x) {
+    if (x < 0) {
         return -1 * reverse(-x);
     }
 
     var arr = x.toString().split('');
     var len = arr.length;
-    if(len<=1) {
+    if (len <= 1) {
         return x;
     }
 
-    var start = 0, end = len-1;
-    while(start < end) {
+    var start = 0, end = len - 1;
+    while (start < end) {
         var tmp = arr[start];
         arr[start] = arr[end];
         arr[end] = tmp;
@@ -28,4 +29,24 @@ var reverse = function(x) {
     }
 
     return parseInt(arr.join(''));
+};
+
+// Solution 2
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    if (x < 0) {
+        return (-1) * reverse(x * (-1));
+    } else if (x === 0) {
+        return 0;
+    } else {
+        var ret = parseInt(x.toString().split('').reverse().join(''));
+        if (ret > Math.pow(2, 31) - 1) {
+            return 0;
+        } else {
+            return ret;
+        }
+    }
 };

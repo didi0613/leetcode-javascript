975ebb62880d4f2eec7c7800e8cb7722e0dcce16
gina0613 <sdi@usc.edu>
2016-06-15 16:50:05 -0700

    Leetcode Easy Round 2

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -2,13 +2,13 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
     var str_array = s.trim().split(/(\s+)/);
     var len = str_array.length;
 
-    if(len === 0 ){
+    if (len === 0) {
         return 0;
     }
 
-    return str_array[len-1].length;
+    return str_array[len - 1].length;
 };

ca5af4e8ab10b4ca181018cd05db51603fdaa834
gina0613 <sdi@usc.edu>
2016-06-15 12:28:21 -0700

    Leetcode Easy Round 2

--- remove-linked-list.js
+++ remove-linked-list.js
@@ -1,3 +1,4 @@
+// Solution 1
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -10,25 +11,62 @@
  * @param {number} val
  * @return {ListNode}
  */
-var removeElements = function(head, val) {
-    if(head === null) {
+var removeElements = function (head, val) {
+    if (head === null) {
         return head;
     }
 
-    if(head.val === val) {
+    if (head.val === val) {
         return removeElements(head.next, val);
     }
 
     var prev = new ListNode(-1);
     prev.next = head;
     var cur = head;
-    while(cur !== null) {
-        if(cur.val === val) {
+    while (cur !== null) {
+        if (cur.val === val) {
             prev.next = cur.next;
             cur = prev.next;
         } else {
             prev = cur;
             cur = cur.next;
+        }
+    }
+
+    return head;
+};
+
+// Solution 2
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    if (head === null) {
+        return head;
+    }
+
+    if (head.val === val) {
+        return removeElements(head.next, val);
+    }
+
+    var node = head;
+    var prev = null;
+    while (node) {
+        if (node.val === val) {
+            prev.next = node.next;
+            node = prev.next;
+        } else {
+            prev = node;
+            node = node.next;
         }
     }
 

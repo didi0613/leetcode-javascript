1b18cabc72003b7b7c429978e3fbf702df62615b
gina0613 <sdi@usc.edu>
2016-06-14 21:57:09 -0700

    Leetcode Easy Round 2

--- excel-sheet-column-title.js
+++ excel-sheet-column-title.js
@@ -2,13 +2,18 @@
  * @param {number} n
  * @return {string}
  */
-var convertToTitle = function(n) {
-    var ret = ""
+var convertToTitle = function (n) {
+    if (n <= 26) {
+        return String.fromCharCode(64 + n);
+    }
 
-    while(n>0) {
-        var cur = (n-1)%26;
-        ret = String.fromCharCode(65+cur) + ret;
-        n = Math.floor((n-1)/26);
+    var ret = "";
+    while (n > 0) {
+        // 因为这里的值是以1为基准的，而不是0，相当于26进制的数都整体减1，才能对应上从0开始的十进制数。
+        n--;
+        var a = n % 26;
+        ret = String.fromCharCode(65 + a) + ret;
+        n = Math.floor(n / 26);
     }
 
     return ret;

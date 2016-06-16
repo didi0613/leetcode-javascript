a4c282e00f0a5b2abe2b9e85d38fcad2ceb13171
gina0613 <sdi@usc.edu>
2016-06-15 17:42:11 -0700

    Leetcode Easy Round 2

--- isomorphic-strings.js
+++ isomorphic-strings.js
@@ -3,17 +3,17 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
+var isIsomorphic = function (s, t) {
     var len1 = s.length, len2 = t.length;
 
-    if(len1 !== len2) {
+    if (len1 !== len2) {
         return false;
     }
 
     var mapA = [];
-    for(var i=0;i<len1;i++) {
-        if(s[i] in mapA) {
-            if(mapA[s[i]] !== t[i]) {
+    for (var i = 0; i < len1; i++) {
+        if (s[i] in mapA) {
+            if (mapA[s[i]] !== t[i]) {
                 return false;
             }
         } else {
@@ -22,9 +22,9 @@ var isIsomorphic = function(s, t) {
     }
 
     var mapB = [];
-    for(var j=0;j<len1;j++) {
-        if(t[j] in mapB) {
-            if(mapB[t[j]] !== s[j]) {
+    for (var j = 0; j < len1; j++) {
+        if (t[j] in mapB) {
+            if (mapB[t[j]] !== s[j]) {
                 return false;
             }
         } else {

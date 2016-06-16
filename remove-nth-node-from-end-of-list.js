923a4eb7b0669ca5f1127c03069e8ea6b7de66b9
gina0613 <sdi@usc.edu>
2016-06-15 17:28:56 -0700

    Leetcode Easy Round 2

--- remove-nth-node-from-end-of-list.js
+++ remove-nth-node-from-end-of-list.js
@@ -10,22 +10,22 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
+var removeNthFromEnd = function (head, n) {
     var runner = head;
     var mark = [];
-    var len=0;
+    var len = 0;
 
-    while(runner !== null) {
+    while (runner !== null) {
         mark[len] = runner;
         runner = runner.next;
         len++;
     }
 
     // The one we will remove mark[len-n]
-    if(len-n === 0) {
+    if (len - n === 0) {
         return head.next;
     } else {
-        mark[len-n-1].next = mark[len-n].next;
+        mark[len - n - 1].next = mark[len - n].next;
         return head;
     }
 

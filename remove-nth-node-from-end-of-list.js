4d95f0dbf20a379744c3231f1024099700bbde73
gina0613 <sdi@usc.edu>
2016-06-15 17:29:23 -0700

    Leetcode Easy Round 2

--- remove-nth-node-from-end-of-list.js
+++ remove-nth-node-from-end-of-list.js
@@ -1,3 +1,4 @@
+// Solution 1
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -29,4 +30,45 @@ var removeNthFromEnd = function (head, n) {
         return head;
     }
 
+};
+
+// solution 2
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    // Create two pointers.
+    // Make them depart n distance
+
+    if (head.next === null || head === null) {
+        return null;
+    }
+
+    var slow = head, fast = head;
+    while (n > 0) {
+        fast = fast.next;
+        n--;
+    }
+
+    // Remove head situation
+    if (fast === null) {
+        return head.next;
+    }
+
+    while (fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+    return head;
 };

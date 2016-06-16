7d75af5c9951a54914c5753ba3f02ace1dddbdb3
gina0613 <sdi@usc.edu>
2016-06-15 19:08:54 -0700

    Leetcode Easy Round 2

--- bulls-and-cows.js
+++ bulls-and-cows.js
@@ -3,32 +3,32 @@
  * @param {string} guess
  * @return {string}
  */
-var getHint = function(secret, guess) {
+var getHint = function (secret, guess) {
     var nums_a = 0, nums_b = 0;
     var hash_a = [], hash_b = [];
 
-    for(var p = 0;p<secret.length;p++) {
-        if(secret[p] in hash_a) {
-                hash_a[secret[p]]++;
-            } else {
-                hash_a[secret[p]] = 1;
+    for (var p = 0; p < secret.length; p++) {
+        if (secret[p] in hash_a) {
+            hash_a[secret[p]]++;
+        } else {
+            hash_a[secret[p]] = 1;
         }
     }
 
     // check the exactly equal situation
-    for(var i=0;i<guess.length;i++) {
-        if(guess[i] === secret[i]) {
+    for (var i = 0; i < guess.length; i++) {
+        if (guess[i] === secret[i]) {
             nums_a++;
-            if(hash_a[secret[i]] > 0) {
+            if (hash_a[secret[i]] > 0) {
                 hash_a[secret[i]]--;
             }
         }
     }
 
     // check the rest of non-paired object
-    for(var ii=0; ii<guess.length;ii++) {
-        if(guess[ii] !== secret[ii]) {
-            if(guess[ii] in hash_a && hash_a[guess[ii]] > 0) {
+    for (var ii = 0; ii < guess.length; ii++) {
+        if (guess[ii] !== secret[ii]) {
+            if (guess[ii] in hash_a && hash_a[guess[ii]] > 0) {
                 nums_b++;
                 hash_a[guess[ii]]--;
             }
--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -5,22 +5,22 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
     // sort from the back
     // last index is m+n-1
 
-    while(m>0 && n>0) {
-        if(nums2[n-1] >= nums1[m-1]) {
-            nums1[m+n-1] = nums2[n-1];
+    while (m > 0 && n > 0) {
+        if (nums2[n - 1] >= nums1[m - 1]) {
+            nums1[m + n - 1] = nums2[n - 1];
             n--;
         } else {
-            nums1[m+n-1] = nums1[m-1];
+            nums1[m + n - 1] = nums1[m - 1];
             m--;
         }
     }
 
-    while(n > 0) {
-        nums1[m+n-1] = nums2[n-1];
+    while (n > 0) {
+        nums1[m + n - 1] = nums2[n - 1];
         n--;
     }
 };

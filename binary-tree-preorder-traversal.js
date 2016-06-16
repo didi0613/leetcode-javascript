55cf20eba969e70a2e8a6aff5da68932ebc58f0a
gina0613 <sdi@usc.edu>
2016-06-16 16:45:07 -0700

    Leetcode Medium Round 1

---
+++ binary-tree-preorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+    var ret = [];
+    preorderTraversalBuilder(root, ret);
+    return ret;
+};
+
+function preorderTraversalBuilder(node, ret) {
+    if (node === null) {
+        return;
+    }
+
+    ret.push(node.val);
+    preorderTraversalBuilder(node.left, ret);
+    preorderTraversalBuilder(node.right, ret);
+}

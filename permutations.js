f83f8afdf1a639f8449e33883afa0a8959a0f184
gina0613 <sdi@usc.edu>
2016-06-17 12:56:00 -0700

    Leetcode Medium Round 1

--- permutations.js
+++ permutations.js
@@ -1,9 +1,31 @@
-// Solution 1
-/*
- * Insert separately
- * [1,2,3]
- * => [1] insert 2
- * => [2,1] [1,2]
- * => insert 3
- * => [3,2,1] [2,3,1] [2,1,3] [3,1,2] [1,3,2] [1,2,3]
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
  */
+var permute = function (nums) {
+    var len = nums.length;
+    if (len === 0) {
+        return [];
+    }
+
+    var ret = [];
+    permuteBuilder(nums, [], [], ret);
+    return ret;
+};
+
+function permuteBuilder(nums, hash, level, ret) {
+    if (level.length === nums.length) {
+        ret.push(level.slice());
+        return;
+    }
+
+    for (var i = 0; i < nums.length; i++) {
+        if (!hash[i]) {
+            level.push(nums[i]);
+            hash[i] = true;
+            permuteBuilder(nums, hash, level, ret);
+            level.pop();
+            hash[i] = false;
+        }
+    }
+}

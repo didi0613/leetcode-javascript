8de185fbd4b64423f61c428dc7f30ab4e9d4549b
gina0613 <sdi@usc.edu>
2016-06-20 16:07:49 -0700

    Leetcode Solutions

--- bulb-switcher.js
+++ bulb-switcher.js
@@ -1,10 +1,27 @@
 // 所有平方数的灯泡都将会是打开的状态
 // 问题简化为 求1到n之间完全平方数的个数
 
+// Solution 1
 /**
  * @param {number} n
  * @return {number}
  */
 var bulbSwitch = function (n) {
     return Math.floor(Math.sqrt(n));
+};
+
+// Solution 2
+// Without using API
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bulbSwitch = function (n) {
+    var ret = 1;
+
+    while (ret * ret <= n) {
+        ret++;
+    }
+
+    return ret - 1;
 };

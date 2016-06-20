9badf9049005e16dea73875ec5f70ee14af2d649
gina0613 <sdi@usc.edu>
2016-06-20 10:45:10 -0700

    Leetcode Medium Round 1

--- nested-list-weight-sum.js
+++ nested-list-weight-sum.js
@@ -1,15 +1,15 @@
-function depthSum(){
-    var arr = [[1,1],2,[1,1]];
-    return helper(arr,1);
+function depthSum() {
+    var arr = [[1, 1], 2, [1, 1]];
+    return helper(arr, 1);
 }
 
 function helper(arr, level) {
     var ret = 0;
-    for(var i=0;i<arr.length;i++) {
-        if(Number.isInteger(arr[i])) {
-            ret += arr[i]*level;
+    for (var i = 0; i < arr.length; i++) {
+        if (Number.isInteger(arr[i])) {
+            ret += arr[i] * level;
         } else {
-            return helper(arr[i], level+1);
+            return helper(arr[i], level + 1);
         }
     }
 

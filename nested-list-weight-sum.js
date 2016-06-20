05fea0bebdbe878788a47b8d2867c0ec045ff238
gina0613 <sdi@usc.edu>
2016-06-20 10:45:38 -0700

    Leetcode Solutions

--- nested-list-weight-sum.js
+++ nested-list-weight-sum.js
@@ -3,13 +3,13 @@ function depthSum() {
     return helper(arr, 1);
 }
 
-function helper(arr, level) {
+function depthSumBuilder(arr, level) {
     var ret = 0;
     for (var i = 0; i < arr.length; i++) {
         if (Number.isInteger(arr[i])) {
             ret += arr[i] * level;
         } else {
-            return helper(arr[i], level + 1);
+            return depthSumBuilder(arr[i], level + 1);
         }
     }
 

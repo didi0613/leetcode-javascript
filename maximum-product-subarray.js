550fcb646e032cc7a2c148916c82321686d5a55d
gina0613 <sdi@usc.edu>
2016-06-20 15:30:11 -0700

    Leetcode Solutions

---
+++ maximum-product-subarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    var len = nums.length;
+    if (len === 0) {
+        return 0;
+    }
+
+    //at least one number
+    var max = [], min = [], ret = nums[0];
+    max[0] = nums[0];
+    min[0] = nums[0];
+
+    for (var i = 1; i < len; i++) {
+        if (nums[i] > 0) {
+            max[i] = Math.max(max[i - 1] * nums[i], nums[i]);
+            min[i] = Math.min(min[i - 1] * nums[i], nums[i]);
+        } else {
+            max[i] = Math.max(min[i - 1] * nums[i], nums[i]);
+            min[i] = Math.min(max[i - 1] * nums[i], nums[i]);
+        }
+    }
+
+    return Math.max(max[i], ret);
+};

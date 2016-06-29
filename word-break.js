09840f761445685ff72f9cffe1a1add0990d3f1d
gina0613 <sdi@usc.edu>
2016-06-29 14:56:08 -0700

    Leetcode solution

---
+++ word-break.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {set<string>} wordDict
+ *   Note: wordDict is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+    var len = s.length;
+
+    var dp = [];
+    dp[0] = true;
+    //dp[i]: from index 0 to position i, if it is a valid word
+    for (var i = 1; i <= len; i++) {
+        for (var j = 0; j < i; j++) {
+            dp[i] = dp[j] && wordDict.has(s.substring(j, i));
+            if (dp[i]) {
+                break;
+            }
+        }
+    }
+
+    return dp[len];
+};

40fa653eacdbdcbf40a8eb8c49d95519767b27a5
gina0613 <sdi@usc.edu>
2016-06-29 14:23:37 -0700

    Leetcode solution

---
+++ serialize-and-deserialize-binary-tree.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function (root) {
+    if (root === null) {
+        return "";
+    }
+
+    var ret = "";
+    ret += root.val;
+    var left = serialize(root.left);
+    var right = serialize(root.right);
+    ret += "," + left;
+    ret += "," + right;
+    return ret;
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function (data) {
+    var arr = data.split(",");
+    var q = [];
+    for (var i in arr) {
+        q.unshift(arr[i]);
+    }
+
+    return deserializeBuilder(q);
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
+
+function deserializeBuilder(q) {
+    var tmp = q.pop();
+    if (tmp.length === 0) {
+        return null;
+    }
+
+    var root = new TreeNode(parseInt(tmp));
+    root.left = deserializeBuilder(q);
+    root.right = deserializeBuilder(q);
+
+    return root;
+}

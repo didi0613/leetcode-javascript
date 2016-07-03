993d44b95aa14363a4ddf23a9be12349edc8dd15
gina0613 <sdi@usc.edu>
2016-07-02 22:55:32 -0700

    Leetcode

--- word-search-ii.js
+++ word-search-ii.js
@@ -1,4 +1,73 @@
 /**
+ * @constructor
+ * Initialize your data structure here.
+ */
+var TrieNode = function (key) {
+    return {
+        key: key,
+        isWord: false
+    }
+};
+
+var Trie = function () {
+    this.root = TrieNode('root');
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ * Inserts a word into the trie.
+ */
+Trie.prototype.insert = function (word) {
+    var tree = this.root;
+    for (var i in word) {
+        var cur = word[i];
+        if (!tree[cur]) {
+            tree[cur] = new TrieNode(cur);
+        }
+        tree = tree[cur];
+    }
+    tree.isWord = true;
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ * Returns if the word is in the trie.
+ */
+Trie.prototype.search = function (word) {
+    var tree = this.root;
+    for (var i in word) {
+        if (!tree[word[i]]) {
+            return false;
+        }
+        tree = tree[word[i]];
+    }
+    if (tree.isWord) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ * Returns if there is any word in the trie
+ * that starts with the given prefix.
+ */
+Trie.prototype.startsWith = function (prefix) {
+    var tree = this.root;
+    for (var i in prefix) {
+        if (!tree[prefix[i]]) {
+            return false;
+        }
+        tree = tree[prefix[i]];
+    }
+    return true;
+};
+
+/**
  * @param {character[][]} board
  * @param {string[]} words
  * @return {string[]}

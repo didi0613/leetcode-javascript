990428c63ae48b8efef4a19221cf9044758ac76c
gina0613 <sdi@usc.edu>
2016-07-15 23:19:46 -0700

    Leetcode Solution

--- container-with-most-water.js
+++ container-with-most-water.js
@@ -1,6 +1,10 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * X轴上两指针的距离right - left为矩形长；
+ * Y轴取两个指针所指的较短边：
+ * Math.min(heights[left], heights[right])作为宽，相乘所得max为最大装水容量。
+ * 将两指针向中间移动，更新max的最大值。
  */
 var maxArea = function (height) {
     var start = 0, end = height.length - 1;

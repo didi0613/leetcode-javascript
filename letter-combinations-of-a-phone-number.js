4cae0e785a450b516c1b68a69a2c07bb3c00f9e3
gina0613 <sdi@usc.edu>
2016-07-16 12:30:48 -0700

    Leetcode Solution

---
+++ letter-combinations-of-a-phone-number.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    var hashmap = {
+        2: ["a", "b", "c"], 3: ["d", "e", "f"], 4: ["g", "h", "i"], 5: ["k", "j", "l"], 6: ["m", "n", "o"],
+        7: ["p", "q", "r", "s"], 8: ["t", "u", "v"], 9: ["w", "x", "y", "z"]
+    };
+    var ret = [];
+    for (var i = 0; i < digits; i++) {
+        if (digits[i] in hashmap) {
+            if (ret.length === 0) {
+                ret.push(hashmap[digits[i]]);
+                continue;
+            }
+
+            // insert into each of the characters in ret
+            for (var j = 0; j < ret.length; j++) {
+                var item = ret.pop();
+                // loop each character in digits
+                for (var k = 0; k < hashmap[digits[i]].length; k++) {
+                    var tmp = item + hashmap[digits[i]][k];
+                    ret.push(tmp);
+                }
+            }
+        }
+    }
+
+    return ret;
+};

f983cf1d060bfc7da6dcffb7db75a8fcddd9c39b
gina0613 <sdi@usc.edu>
2016-07-16 15:35:51 -0700

    Leetcode Solution

--- generate-parentheses.js
+++ generate-parentheses.js
@@ -1,6 +1,10 @@
 /**
  * @param {number} n
  * @return {string[]}
+ * 假设：左括符个数用left表示，右括符个数用right表示
+ * 递归终止条件： left = right = n
+ * 先要生成左括符，可以生成左括符的条件：left < n
+ * 生成右括符条件： right < left
  */
 var generateParenthesis = function (n) {
     var ret = [];

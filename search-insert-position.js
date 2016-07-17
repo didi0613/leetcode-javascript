0ab56245a6c2839bd77488686fd0e73d3db29e19
gina0613 <sdi@usc.edu>
2016-07-16 21:20:57 -0700

    Leetcode Solution

---
+++ search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    if (target < nums[0]) {
+        return 0;
+    } else if (target > nums[nums.length - 1]) {
+        return nums.length;
+    }
+
+    for (var i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === target) {
+            return i;
+        }
+        if (nums[i] < target && nums[i + 1] > target) {
+            return i + 1;
+        }
+    }
+};

7ca7963eab7d598e47dc9fd5664452c4f9b98631
gina0613 <sdi@usc.edu>
2016-07-17 13:31:24 -0700

    Leetcode Solution

---
+++ merge-intervals.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
+var merge = function (intervals) {
+    // sort the intervals
+    var ret = [];
+
+    intervals.sort(function (a, b) {
+        if (a.start === b.start) {
+            return a.end - b.end;
+        } else {
+            return a.start - b.start;
+        }
+    });
+
+    var pre = intervals[0];
+    for (var i = 1; i < intervals.length; i++) {
+        var cur = intervals[i];
+        if (pre.end >= cur.start) {
+            pre.end = Math.max(pre.end, cur.end);
+            intervals.splice(i, 1);
+            i--;
+        } else {
+            pre = cur;
+        }
+    }
+
+    return intervals;
+};

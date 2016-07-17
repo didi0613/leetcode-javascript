c8ab09ae9a354b8641936e1d1e773cbbc3067d9e
gina0613 <sdi@usc.edu>
2016-07-17 12:52:41 -0700

    Leetcode Solution

--- rotate-image.js
+++ rotate-image.js
@@ -5,6 +5,7 @@
 var rotate = function (matrix) {
     var n = matrix.length;
     for (var i = 0; i < n / 2; ++i)
+        // 由最外围一圈，向里一圈圈收缩
         for (var j = i; j < n - 1 - i; ++j) {
             var t = matrix[i][j];
             matrix[i][j] = matrix[n - 1 - j][i];

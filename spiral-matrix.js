68a08c2885b042fd475a2f1c2552d1550f9b2984
gina0613 <sdi@usc.edu>
2016-07-17 13:12:44 -0700

    Leetcode Solution

---
+++ spiral-matrix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+    var ret = [];
+    if (matrix.length === 0) {
+        return ret;
+    }
+
+    var rowBegin = 0, rowEnd = matrix.length - 1;
+    var colBegin = 0, colEnd = matrix[0].length - 1;
+
+    while (rowBegin <= rowEnd && colBegin <= colEnd) {
+        //left to right
+        for (var j = colBegin; j <= colEnd; j++) {
+            ret.push(matrix[rowBegin][j]);
+        }
+        rowBegin++;
+
+        //right to bottom
+        for (j = rowBegin; j <= rowEnd; j++) {
+            ret.push(matrix[j][colEnd]);
+        }
+        colEnd--;
+
+        if (rowBegin <= rowEnd) {
+            // Traverse Left
+            for (j = colEnd; j >= colBegin; j--) {
+                ret.push(matrix[rowEnd][j]);
+            }
+        }
+        rowEnd--;
+
+        if (colBegin <= colEnd) {
+            // Traver Up
+            for (j = rowEnd; j >= rowBegin; j--) {
+                ret.push(matrix[j][colBegin]);
+            }
+        }
+        colBegin++;
+    }
+
+    return ret;
+};

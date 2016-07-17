ac39170708479207cd70797557c30c2e4c540c7c
gina0613 <sdi@usc.edu>
2016-07-17 13:03:36 -0700

    Leetcode Solution

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -4,16 +4,13 @@
  */
 var maxSubArray = function (nums) {
     var len = nums.length;
-    if (len === 0) {
-        return 0;
-    }
-
-    var ret = nums[0], max = [];
+    var max = [];
+    var ret = nums[0];
     max[0] = nums[0];
 
     for (var i = 1; i < len; i++) {
         max[i] = Math.max(max[i - 1] + nums[i], nums[i]);
-        ret = Math.max(ret, max[i]);
+        ret = Math.max(max[i], ret);
     }
 
     return ret;

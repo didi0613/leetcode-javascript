15cc1ad80014a11c31e15d36dcc106f1fcc8cfa4
gina0613 <sdi@usc.edu>
2016-07-16 17:36:24 -0700

    Leetcode Solution

--- longest-valid-parentheses.js
+++ longest-valid-parentheses.js
@@ -6,6 +6,7 @@
 var longestValidParentheses = function (s) {
     var stack = [];
     var ret = 0;
+    // 起始索引
     var left = 0;
     for (var i = 0; i < s.length; i++) {
         if (s[i] === "(") {
@@ -14,11 +15,14 @@ var longestValidParentheses = function (s) {
             if (stack.length !== 0) {
                 stack.pop();
                 if (stack.length === 0) {
+                    // 说明当前已经匹配到了起始索引出，从起始索引开始计算长度
                     ret = Math.max(ret, i - left + 1);
                 } else {
+                    // 说明当前还在连续匹配串内，从当前栈顶元素索引开始计算长度即可
                     ret = Math.max(ret, i - stack[stack.length - 1]);
                 }
             } else {
+                //当前匹配失效,且不是起始索引,移动起始索引
                 left = i + 1;
             }
         }

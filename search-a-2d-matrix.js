ace92019db2a1bfa17ee7aff5a70186c96125871
gina0613 <sdi@usc.edu>
2016-07-17 23:44:08 -0700

    Leetcode Solution

--- search-a-2d-matrix.js
+++ search-a-2d-matrix.js
@@ -6,5 +6,22 @@
  * 那么如果把数组一字排开就是正常的有序数组二分搜索了.
  */
 var searchMatrix = function (matrix, target) {
+    if (matrix.length === 0 || matrix[0].length === 0) {
+        return false;
+    }
 
+    var row = matrix.length, col = matrix[0].length;
+    var low = 0, high = row * col - 1;
+    while (low <= high) {
+        var mid = parseInt((low + high) / 2);
+        if (matrix[parseInt(mid / col)][mid % col] === target) {
+            return true;
+        } else if (matrix[parseInt(mid / col)][mid % col] < target) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return false;
 };

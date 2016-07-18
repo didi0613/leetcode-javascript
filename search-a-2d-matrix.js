79611139bab3b73a189c8a946814d752319f6e8d
gina0613 <sdi@usc.edu>
2016-07-17 23:39:50 -0700

    Leetcode Solution

---
+++ search-a-2d-matrix.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ * 把matrix看成一个m*n的数组,
+ * 那么如果把数组一字排开就是正常的有序数组二分搜索了.
+ */
+var searchMatrix = function (matrix, target) {
+
+};

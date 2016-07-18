177aa81e00b8c1fcd4259d91997299db6bc82b59
gina0613 <sdi@usc.edu>
2016-07-18 00:06:31 -0700

    Leetcode Solution

---
+++ search-in-rotated-sorted-array-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var search = function (nums, target) {
+    var left = 0, right = nums.length - 1;
+    while (left <= right) {
+        var mid = parseInt((left + right) / 2);
+        if (nums[mid] === target) {
+            return true;
+        }
+
+        if (nums[mid] > nums[left]) {
+            if (nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else if (nums[mid] < nums[left]) {
+            if (nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            left++;
+        }
+    }
+
+    return false;
+};

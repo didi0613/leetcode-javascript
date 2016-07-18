6e8cab0b1f202fce20836c2b15dbae477684d351
gina0613 <sdi@usc.edu>
2016-07-18 13:31:07 -0700

    Leetcode Solution

---
+++ palindrome-partitioning-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 本题的思路是：
+ * 建立boolean二维数组dp[i][j]，存放字符串s从第i到第j位是否为回文的布尔值；
+ * 同时建立整型数组min[i]，存放从最后一位到第i位都是回文串的最小切割次数。
+ * 然后从字符串s的最后一个字符开始，向前进行分析。
+ */
+var minCut = function (s) {
+
+};

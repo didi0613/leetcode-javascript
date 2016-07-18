cfc4a323b9824a359e28f764b3a1492f5c8f462d
gina0613 <sdi@usc.edu>
2016-07-18 13:18:13 -0700

    Leetcode Solution

--- longest-consecutive-sequence.js
+++ longest-consecutive-sequence.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var longestConsecutive = function (nums) {
-    nums.sort();
-    var count = new Array(num.length);
+    nums.sort(function (a, b) {
+        return a - b;
+    });
+    var count = new Array(nums.length);
     count[0] = 1;
     var max = 1;
-    for (var i = 1; i < num.length; i++) {
-        if (num[i] == num[i - 1]) count[i] = count[i - 1];
-        else if (num[i] == num[i - 1] + 1) count[i] = count[i - 1] + 1;
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i] == nums[i - 1]) count[i] = count[i - 1];
+        else if (nums[i] == nums[i - 1] + 1) count[i] = count[i - 1] + 1;
         else count[i] = 1;
         max = Math.max(max, count[i]);
     }

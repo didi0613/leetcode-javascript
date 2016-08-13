c73b06cb2b9394b517a57df4df7989cab68ccf47
gina0613 <sdi@usc.edu>
2016-08-13 13:59:31 -0700

    Leetcode

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -39,3 +39,41 @@ var mergeTwoLists = function (l1, l2) {
 
 // Time Complexity: O(m+n)
 // Space Complexity: O(m+n)
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+    var ret = new ListNode(-1);
+    var cur = ret;
+
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+        cur = cur.next;
+    }
+
+    if (l1) {
+        cur.next = l1;
+    }
+
+    if (l2) {
+        cur.next = l2;
+    }
+
+    return ret.next;
+};

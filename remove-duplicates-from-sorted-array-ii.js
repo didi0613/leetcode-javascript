d3bddabe619e252def8c7a86402094ba5463cd3d
gina0613 <sdi@usc.edu>
2016-08-16 12:53:05 -0700

    Leetcode

---
+++ remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    if (nums.length == 0) {
+        return 0;
+    }
+    var index = 0, count = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (i == 0 || nums[i] != nums[i - 1]) count = 1;
+        else count++;
+        if (count <= 2) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+};

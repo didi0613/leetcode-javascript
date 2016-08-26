dd7d136aafc72b4d6b11d2c661a354cbe2d5f665
gina0613 <sdi@usc.edu>
2016-08-25 18:37:43 -0700

    Leetcode

--- top-k-frequent-elements.js
+++ top-k-frequent-elements.js
@@ -4,15 +4,10 @@
  * @return {number[]}
  */
 var topKFrequent = function (nums, k) {
-    var len = nums.length;
-    var ret = [];
-    if (len === 0) {
-        return 0;
-    }
-
-    // get numbers => frequency dictionary
     var hashmap = {};
-    for (var i in nums) {
+
+    // key => frequency
+    for (var i = 0; i < nums.length; i++) {
         if (hashmap[nums[i]]) {
             hashmap[nums[i]]++;
         } else {
@@ -20,6 +15,7 @@ var topKFrequent = function (nums, k) {
         }
     }
 
+    // frequency => key
     // get all the frequency => number list dictionary
     var freqHashmap = {};
     var hashmapKeys = Object.keys(hashmap);
@@ -32,11 +28,14 @@ var topKFrequent = function (nums, k) {
         freqHashmap[freq].push(key);
     }
 
-    // get the top k answer
-    for (i = len, j = 0; k > 0; k--) {
-        // 如果当前的i在freqHashmap[i]中已经遍历完了，则进入到下一个i
-        for (; !freqHashmap[i] || freqHashmap[i].length === j; j = 0, i--);
-        ret.push(parseInt(freqHashmap[i][j++]));
+    // get the top k
+    var ret = [];
+    var keys = Object.keys(freqHashmap);
+    for (var j = keys.length - 1; j >= 0; j--) {
+        for (var p = 0; p < freqHashmap[keys[j]].length && k > 0; p++) {
+            ret.push(parseInt(freqHashmap[keys[j]][p]));
+            k--;
+        }
     }
 
     return ret;

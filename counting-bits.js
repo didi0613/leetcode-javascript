4353747d906f04106071b7a7cd9c0c3faf132ec6
gina0613 <sdi@usc.edu>
2016-08-29 09:31:30 -0700

    Leetcode

--- counting-bits.js
+++ counting-bits.js
@@ -6,7 +6,7 @@ var countBits = function (num) {
     var ret = [0];
 
     for (var i = 1; i <= num; i++) {
-        ret.push(countOnes(num.toString(2)));
+        ret.push(countOnes(i.toString(2)));
     }
 
     return ret;

a817fe64ddaf53df4db46e7f7d76e42f56447b3a
gina0613 <sdi@usc.edu>
2016-08-30 09:28:31 -0700

    leetcode solution

--- counting-bits.js
+++ counting-bits.js
@@ -22,3 +22,24 @@ function countOnes(num) {
     }
     return ret;
 }
+
+/**
+ * @param {number} num
+ * @return {number[]}
+ */
+var countBits = function (num) {
+    var ret = [0];
+
+    for (var i = 1; i <= num; i++) {
+        // 从1开始，
+        // 遇到偶数时, 其1的个数和该偶数除以2得到的数字的1的个数相同，
+        // 遇到奇数时，其1的个数等于该奇数除以2得到的数字的1的个数再加
+        if (i % 2 === 0) {
+            ret.push(ret[parseInt(i / 2)]);
+        } else {
+            ret.push(ret[parseInt(i / 2)] + 1);
+        }
+    }
+
+    return ret;
+};

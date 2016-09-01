f2baa033bb8b687cb1d8af6926e7fb3fadbe8123
gina0613 <sdi@usc.edu>
2016-08-31 22:09:51 -0700

    leetcode solution

--- count-numbers-with-unique-digits.js
+++ count-numbers-with-unique-digits.js
@@ -2,28 +2,21 @@
  * @param {number} n
  * @return {number}
  */
-// DP Solution
 var countNumbersWithUniqueDigits = function (n) {
     if (n === 0) {
         return 1;
     }
 
-    n = Math.min(n, 10);
-
     // fn: number of unique digits of length n
     var f = [];
     f[0] = 1;
     f[1] = 9;
-    var i = 2;
-    var availableNumbers = 9;
     var sum = f[0] + f[1];
-
-    while (i <= n && availableNumbers > 0) {
+    var availableNumbers = 9;
+    for (var i = 2; i <= n && availableNumbers > 0; i++) {
         f[i] = f[i - 1] * availableNumbers;
         sum += f[i];
-        i++;
         availableNumbers--;
     }
-
     return sum;
 };

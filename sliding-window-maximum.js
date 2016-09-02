fb94bde858ed35897cdab307847076f73ebbf1cb
gina0613 <sdi@usc.edu>
2016-09-02 12:05:49 -0700

    leetcode solution

--- sliding-window-maximum.js
+++ sliding-window-maximum.js
@@ -6,23 +6,24 @@
 var maxSlidingWindow = function (nums, k) {
     // leftmax[i]: 0 ~ i max value
     // rightmax[i]: i ~ nums.length-1 max value
-    if (nums.length === 0) {
+    var len = nums.length;
+    if (len === 0) {
         return [];
     }
 
-    var leftmax = new Array(nums.length);
-    var rightmax = new Array(nums.length);
+    var leftmax = new Array(len);
+    var rightmax = new Array(len);
 
     leftmax[0] = nums[0];
-    rightmax[nums.length - 1] = nums[nums.length - 1];
+    rightmax[len - 1] = nums[len - 1];
 
-    for (var i = 1; i < nums.length; i++) {
+    for (var i = 1; i < len; i++) {
         leftmax[i] = (i % k === 0) ? nums[i] : Math.max(leftmax[i - 1], nums[i]);
-        rightmax[nums.length - 1 - i] = ((nums.length - 1 - i) % k === 0) ? nums[nums.length - 1 - i] : Math.max(rightmax[nums.length - i], nums[nums.length - 1 - i]);
+        rightmax[len - 1 - i] = ((len - 1 - i) % k === 0) ? nums[len - 1 - i] : Math.max(rightmax[len - i], nums[len - 1 - i]);
     }
 
     var ret = [];
-    for (var i = 0; i < nums.length - k + 1; i++) {
+    for (var i = 0; i < len - k + 1; i++) {
         ret[i] = Math.max(leftmax[i + k - 1], rightmax[i]);
     }
     return ret;

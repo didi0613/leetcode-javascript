4f631af03122437d71ca91543b9957c7faa14483
gina0613 <sdi@usc.edu>
2016-09-03 12:13:10 -0700

    leetcode

--- maximum-product-of-word-lengths.js
+++ maximum-product-of-word-lengths.js
@@ -4,24 +4,19 @@
  */
 var maxProduct = function (words) {
     var max = 0;
-    var bitDict = [];
+    var mask = [];
     for (var i = 0; i < words.length; i++) {
         for (var j = 0; j < words[i].length; j++) {
-            bitDict[i] |= Math.pow(2, words[i][j].charCodeAt() - 'a'.charCodeAt());
+            mask[i] |= Math.pow(2, words[i][j].charCodeAt() - 'a'.charCodeAt());
         }
     }
 
     for (i = 0; i < words.length; i++) {
-        for (j = 0; j < words.length; j++) {
-            if (i !== j && !hasSharedLetter(i, j)) {
+        for (j = 0; j < i; j++) {
+            if ((mask[i] & mask[j]) === 0) {
                 max = Math.max(max, words[i].length * words[j].length);
             }
         }
     }
     return max;
-
-    function hasSharedLetter(index1, index2) {
-        if ((bitDict[index1] & bitDict[index2]) === 0) return false;
-        return true;
-    }
 };

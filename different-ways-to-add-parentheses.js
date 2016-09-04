a7cafbecfb84c2abf5b95a4206b18a155dac4375
gina0613 <sdi@usc.edu>
2016-09-04 12:04:26 -0700

    leetcode

---
+++ different-ways-to-add-parentheses.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} input
+ * @return {number[]}
+ */
+var diffWaysToCompute = function (input) {
+    return compute(input);
+};
+
+function compute(str) {
+    var res = [];
+    var regex = /\+|\-|\*/;
+
+    if (!regex.test(str)) {
+        return [parseInt(str)];
+    }
+    for (var i = 0; i < str.length; i++) {
+        if (regex.test(str[i])) {
+            var left = compute(str.substring(0, i));
+            var right = compute(str.substring(i + 1));
+
+            for (var j = 0; j < left.length; j++) {
+                for (var k = 0; k < right.length; k++) {
+                    if (str[i] === '+') {
+                        res.push(parseInt(left[j] + right[k]));
+                    } else if (str[i] === '-') {
+                        res.push(parseInt(left[j] - right[k]));
+                    } else if (str[i] === '*') {
+                        res.push(parseInt(left[j] * right[k]));
+                    }
+                }
+            }
+        }
+    }
+    return res;
+}

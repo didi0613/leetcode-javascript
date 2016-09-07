4835f716bcbbb3189c1f3443dd58cd82a97f5f32
gina0613 <sdi@usc.edu>
2016-09-07 09:09:59 -0700

    leetcode

--- sort-colors.js
+++ sort-colors.js
@@ -3,21 +3,21 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    var hashmap = {};
-    for (var i in nums) {
-        if (nums[i] in hashmap) {
+    var hashmap = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (hashmap[nums[i]]) {
             hashmap[nums[i]]++;
         } else {
             hashmap[nums[i]] = 1;
         }
     }
 
-    var k = 0;
+    var index = 0;
     for (var j in hashmap) {
         while (hashmap[j] > 0) {
+            nums[index] = parseInt(j);
+            index++;
             hashmap[j]--;
-            nums[k++] = parseInt(j);
         }
-        j++;
     }
 };
